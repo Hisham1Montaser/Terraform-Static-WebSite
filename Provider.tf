bb7157ceaffde60665d36f795f8a49a397966f74
Hisham Montaser <hisham.montaser@outlook.com>
2024-09-24 00:38:20 +0300

    initial

--- Provider.tf
+++ Provider.tf
@@ -10,4 +10,6 @@ terraform {
 provider "azurerm" {
   features {}
   subscription_id = "e43ccb45-8ab5-48fe-b42b-1ecd47ed0f76"
+  tenant_id       = "3da777c4-735a-4f58-976d-377cf571b4ff"
+  client_id       = "4a1ef722-16be-4c7a-afc0-5f51dda09137"
 }
